a5f0da26e2272da1d9625a0e65b80d7c9134584f
Chukwuleta Tobechi <47084273+TChukwuleta@users.noreply.github.com>
2026-07-27 07:30:45 +0100

    onboarding wrap up

--- Plugins/BTCPayServer.Plugins.Tando/Plugin.cs
+++ Plugins/BTCPayServer.Plugins.Tando/Plugin.cs
@@ -18,7 +18,7 @@ public class Plugin : BaseBTCPayServerPlugin
 
     public override void Execute(IServiceCollection services)
     {
-        services.AddSingleton<IUIExtension>(new UIExtension("TandoPluginHeaderNav", "header-nav"));
+        /*services.AddSingleton<IUIExtension>(new UIExtension("TandoPluginHeaderNav", "header-nav"));*/
         services.AddSingleton<IUIExtension>(new UIExtension("TandoServerNav", "server-nav"));
         services.AddHostedService<ApplicationPartsLogger>();
         services.AddSingleton<TandoSubscriptionService>();
